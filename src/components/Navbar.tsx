c60c02e299857a3f170932860849322bab779bfd
TonmoyX <142309932+TonmoyX@users.noreply.github.com>
2026-06-13 19:52:53 +0600

    nav logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <Link to="/pricing" className="hover:text-slate-300 transition">Pricing</Link>
           </div>
 
-          <div className="flex items-center gap-3">
+          <div className="hidden md:flex items-center gap-3">
             
             <button onClick={() => navigate('/auth/signin')} className="px-6 py-2 bg-indigo-600 active:scale-95 hover:bg-indigo-700 transition rounded">
               Get started
@@ -34,12 +34,18 @@ const Navbar = () => {
 
          {menuOpen && (
           <div className="fixed inset-0 z-[100] bg-black/60 text-white backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-300">
+
             <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
             <Link to="/projects" onClick={() => setMenuOpen(false)}>My Projects</Link>
             <Link to="/community" onClick={() => setMenuOpen(false)}>Community</Link>
             <Link to="/pricing" onClick={() => setMenuOpen(false)}>Pricing</Link>
+            <div className="md:flex items-center gap-3">
             
-            <button className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-slate-100 hover:bg-slate-200 transition text-black rounded-md flex" onClick={() => setMenuOpen(false)} >
+            <button onClick={() => navigate('/auth/signin')} className="px-6 py-2 bg-indigo-600 active:scale-95 hover:bg-indigo-700 transition rounded">
+              Get started
+            </button>
+          </div>
+            <button className="px-13 py-2 bg-red-600 active:scale-95 hover:bg-red-700 transition rounded" onClick={() => setMenuOpen(false)} >
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
             </button>
           </div>
